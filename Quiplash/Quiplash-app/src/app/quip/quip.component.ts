2fcbe5effa073871f6fd4f4959ae8e5900d798ef
evan-dimartinis <60354368+evan-dimartinis@users.noreply.github.com>
2020-04-12 10:18:38 -0400

    lab 8 attempt 1

--- Quiplash/Quiplash-app/src/app/quip/quip.component.ts
+++ Quiplash/Quiplash-app/src/app/quip/quip.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-quip',
@@ -12,16 +12,28 @@ export class QuipComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  new_answers = [];
-  old_answers = ["evan", "dana"];
+  new_answers = [{user: "Alden", answer: "Alden answer", user_votes: 0}];
+  old_answers = [{name: "evan", answer: "this is my answer"}, {name: "Leah", answer: "Another answer"}];
   submitmessage = "";
   user_answer = "";
   user_name = ""
+  success_message = ""
+  voting_results = [{name: "evan", count: 0}, {name: "Leah", count: 1}]
 
   addAnswer() {
     this.submitmessage = "Your answer has been submitted";
     this.new_answers.push({user: this.user_name, answer: this.user_answer, user_votes: 0});
+    this.success_message = "Answer successfully inputted!"
     console.log(this.new_answers)
+  }
+
+  voteForAnswer(username) {
+    for (var i = 0; i < this.voting_results.length; i++) {
+      if (this.voting_results[i].name == username) {
+        this.voting_results[i].count += 1
+      }
+    }
+    console.log(this.voting_results)
   }
 
 }
